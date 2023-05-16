d9ce2d781ff878ee1d5c472d867ef120901004e4
SaurabhTamta <133592824+SaurabhTamta@users.noreply.github.com>
2023-05-16 20:04:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content',{ origin: 'right' });
 
 
 const typed = new Typed('.multiple-text',{
-    strings : ['Frontend Developer','Qualcommn','Intel'],
+    strings : ['Frontend Developer','Engineer','Aim-->Intel'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
